3108a97a159ddd7b7b5e1646cd3bce989f98c734
watson21 <imnova1212@gmail.com>
2022-06-20 14:06:02 +0700

    complete convert labelme2VOC

--- convert_labelme2VOC.py
+++ convert_labelme2VOC.py
@@ -1,6 +1,6 @@
 from glob import glob
 from typing import List, Tuple
-import os, json
+import os, json, re
 import os.path as osp
 import xml.etree.ElementTree as ET
 # from xml.dom.minidom import parseString
@@ -26,19 +26,19 @@ def get_shape_info(jsonp:str) -> List[Tuple[int, int, int, int, str]]:
         
         #NOTE points
         points = np.array(shape['points'])
-        xmin = points[:, 0].min()
-        ymin = points[:, 1].min()
-        xmax = points[:, 0].max()
-        ymax = points[:, 1].max()
+        xmin = round(points[:, 0].min())
+        ymin = round(points[:, 1].min())
+        xmax = round(points[:, 0].max())
+        ymax = round(points[:, 1].max())
         boxes.append((xmin, ymin, xmax, ymax, lbl))
     
     return boxes
     
 def save_file_xml(img:np.ndarray, abspath:str, folder_save:str, boxes:list) -> None:
-    r''' img             opencv
-    abspath         đường dẫn đầy đủ của ảnh
+    r"""    img     opencv
+        abspath     đường dẫn đầy đủ của ảnh
     folder_save     là folder để save
-    boxes           là list các box theo thứ tự xmin, ymin, xmax, ymax           '''
+          boxes     là list các box theo thứ tự xmin, ymin, xmax, ymax """ 
     
     basename = osp.basename(abspath)
     annotation = ET.Element("annotation")
@@ -62,7 +62,7 @@ def save_file_xml(img:np.ndarray, abspath:str, folder_save:str, boxes:list) -> N
 
     for box in boxes:
         obj = ET.SubElement(annotation, "object")
-        ET.SubElement(obj, "name").text = str(box[4]).strip()
+        ET.SubElement(obj, "name").text = str(box[4]).strip()   #NOTE classname
         
         #NOTE <actions>
         # actions = ET.SubElement(obj, "actions")
@@ -89,12 +89,12 @@ def save_file_xml(img:np.ndarray, abspath:str, folder_save:str, boxes:list) -> N
 
         #NOTE <bndbox>
         bndbox = ET.SubElement(obj, "bndbox")
-        ET.SubElement(bndbox, "xmin").text = str(box[0]).strip()
-        ET.SubElement(bndbox, "ymin").text = str(box[1]).strip()
-        ET.SubElement(bndbox, "xmax").text = str(box[2]).strip()
-        ET.SubElement(bndbox, "ymax").text = str(box[3]).strip()
+        ET.SubElement(bndbox, "xmin").text = str(box[0])
+        ET.SubElement(bndbox, "ymin").text = str(box[1])
+        ET.SubElement(bndbox, "xmax").text = str(box[2])
+        ET.SubElement(bndbox, "ymax").text = str(box[3])
     
-    abspath_xml = osp.join(folder_save, basename.rsplit(".", maxsplit=1)[0] + ".xml")
+    abspath_xml = osp.join(folder_save, basename.rsplit('.', maxsplit=1)[0] + '.xml')
     # tree = ET.ElementTree(annotation)
     # ET.indent(tree, space="\t", level=0)
     # tree.write(name_xml)
@@ -107,16 +107,21 @@ def save_file_xml(img:np.ndarray, abspath:str, folder_save:str, boxes:list) -> N
     with open(abspath_xml, 'w') as f:
         f.write(xmlstr)
 
-def create_labeltxt(datafolder:str):
-    jsons = glob(os.path.join(datafolder, '*.json'))    
-    for jsonpath in jsons:
-        get_shape_info(jsonpath)
 
-    with open('labels.txt', 'w') as f:
+def convert_Labelme2VOC(datafolder:str):
+    jsons = glob(osp.join(datafolder, '*.json'))
+    for js in jsons:
+        boxes = get_shape_info(js)
+        imgp = re.sub('.json$', '.jpg', js)
+        save_file_xml(None, imgp, datafolder, boxes)
+        print(js)
+    
+    #NOTE create labels.txt contain label name of dataset
+    with open(osp.join(datafolder, 'labels.txt'), 'w') as f:
         f.write('\n'.join(labels))
 
 
 if __name__ == '__main__':
-    datafolder = '/home/agent/Documents/graph/GNN_introduction/dataset'
-
-    get_shape_info('dataset/0001.json')
+    datafolder = '/home/agent/Documents/graph/GNN/dataset'
+    convert_Labelme2VOC(datafolder)
+    
