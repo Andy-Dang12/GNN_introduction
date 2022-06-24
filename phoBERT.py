c0a62077e7c3d79a5760501c6406d121a646d3b5
watson21 <imnova1212@gmail.com>
2022-06-24 14:30:10 +0700

    phoBERT

---
+++ phoBERT.py
@@ -0,0 +1,24 @@
+import torch
+from transformers import AutoModel, AutoTokenizer
+from transformers.modeling_outputs import BaseModelOutputWithPoolingAndCrossAttentions
+
+phobert = AutoModel.from_pretrained("vinai/phobert-base")
+tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base")
+
+@torch.no_grad()
+def word2vec(sentence:str) -> torch.Tensor:
+    input_ids = torch.tensor([tokenizer.encode(sentence)])
+    features = phobert(input_ids)  # Models outputs are now tuples
+    return features['pooler_output']
+
+
+if __name__ == '__main__':
+# INPUT TEXT MUST BE ALREADY WORD-SEGMENTED!
+    sentence = 'Chúng_tôi là những nghiên_cứu_viên .'  
+
+    input_ids = torch.tensor([tokenizer.encode(sentence)])
+
+    with torch.no_grad():
+        features:BaseModelOutputWithPoolingAndCrossAttentions \
+            = phobert(input_ids)  # Models outputs are now tuples
+
