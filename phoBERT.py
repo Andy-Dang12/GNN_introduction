235fd99ab2df47f4f5aa3b1760b4f56dc8f3d7a1
watson21 <imnova1212@gmail.com>
2022-07-07 17:41:52 +0700

    preprocessing text punctuation

--- phoBERT.py
+++ phoBERT.py
@@ -1,24 +1,35 @@
-import torch
+import torch, re
+from string import punctuation
 from transformers import AutoModel, AutoTokenizer
 from transformers.modeling_outputs import BaseModelOutputWithPoolingAndCrossAttentions
 
 phobert = AutoModel.from_pretrained("vinai/phobert-base")
 tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base")
 
+pattern = '[{}]'.format(punctuation)
+def preprocess_text(text:str) -> str:
+    r"""
+    xử lý loại bỏ các ký tự đặc biệt trong text
+    ví dụ EMA::IL: -> email
+    """
+    return re.sub(pattern, '', text.lower(), count=len(text)).strip()
+
+
 @torch.no_grad()
 def word2vec(sentence:str) -> torch.Tensor:
+    sentence = preprocess_text(sentence)
     input_ids = torch.tensor([tokenizer.encode(sentence)])
     features = phobert(input_ids)  # Models outputs are now tuples
     return features['pooler_output'].view(-1) #convert to 1d-tensor
 
 
 if __name__ == '__main__':
-# INPUT TEXT MUST BE ALREADY WORD-SEGMENTED!
     sentence = 'Chúng_tôi là những nghiên_cứu_viên .'  
 
-    input_ids = torch.tensor([tokenizer.encode(sentence)])
+    # input_ids = torch.tensor([tokenizer.encode(sentence)])
 
-    with torch.no_grad():
-        features:BaseModelOutputWithPoolingAndCrossAttentions \
-            = phobert(input_ids)  # Models outputs are now tuples
+    # with torch.no_grad():
+    #     features:BaseModelOutputWithPoolingAndCrossAttentions \
+    #         = phobert(input_ids)  # Models outputs are now tuples
+    
 
